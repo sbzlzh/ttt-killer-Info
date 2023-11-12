9784d97861d2c546654c75fb5e2d164f641c21bb
Matt <49911006+NovaDiablox@users.noreply.github.com>
2023-11-13 00:47:49 +0300

    Turkish Language
    Turkish Language

---
+++ gamemodes/terrortown/gamemode/lang/ttt_killer_tr.lua
@@ -0,0 +1,18 @@
+local L = LANG.CreateLanguage("Türkçe")
+
+L.killer_suicide = "Kendin diye biri tarafından öldürüldün..."
+L.killer_burned = "Yandın!"
+L.killer_prop = "Fiziksel hasar alarak öldürüldün.."
+L.killer_ply_innocent = "Masum"
+L.killer_ply_traitor = "Hain"
+L.killer_ply_detective = "Dedektif"
+
+L.killer_fell = "Ölümüne düşerek öldün."
+L.killer_water = "Boğuldun."
+L.killer_unknown = "Nasıl öldüğün belli değil."
+L.killer_unknowns = "bilinmeyen"
+
+L.killer_you = "Tarafından öldürüldün"
+L.killer_yous = " "
+L.killer_comma = ", rol: "
+L.killer_exclamation = "!"
