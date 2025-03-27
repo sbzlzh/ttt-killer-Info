577aabd5d8a131b6bc1b96abaeaf587c721e2719
随波逐流 <2247564152@qq.com>
2025-03-27 14:25:28 +0800

    update

--- lua/autorun/client/cl_death_notifier.lua
+++ lua/autorun/client/cl_death_notifier.lua
@@ -1,19 +1,15 @@
-if engine.ActiveGamemode() ~= "terrortown" and gmod.GetGamemode().Name ~= "Trouble in Terrorist Town" then 
-    return 
-end
-
+if engine.ActiveGamemode() ~= "terrortown" and gmod.GetGamemode().Name ~= "Trouble in Terrorist Town" then return end
 if CLIENT then
     net.Receive("ClientDeathNotify", function()
-		local TraitorColor = Color(255,0,0)
-		local InnoColor = Color(0,255,0)
-		local DetectiveColor = Color(0,0,255)
-		local NameColor = Color(142,68,173)
-		local UnknownColor = Color(152,48,196)
-		local White = Color(236,240,241)
-		local name = net.ReadString()
-		local role =  tonumber(net.ReadString())
-		local reason = net.ReadString()
-
+        local TraitorColor = Color(255, 0, 0)
+        local InnoColor = Color(0, 255, 0)
+        local DetectiveColor = Color(0, 0, 255)
+        local NameColor = Color(142, 68, 173)
+        local UnknownColor = Color(152, 48, 196)
+        local White = Color(236, 240, 241)
+        local name = net.ReadString()
+        local role = tonumber(net.ReadString())
+        local reason = net.ReadString()
         if role == ROLE_INNOCENT then
             col = InnoColor
             role = LANG.GetTranslation("killer_ply_innocent")
@@ -35,7 +31,7 @@ if CLIENT then
         elseif reason == "prop" then
             chat.AddText(NameColor, LANG.GetTranslation("killer_prop"))
         elseif reason == "ply" then
-            chat.AddText( NameColor, LANG.GetTranslation("killer_you"), White, LANG.GetTranslation("killer_yous"),col,name,White,LANG.GetTranslation("killer_comma"),col,role, LANG.GetTranslation("killer_exclamation"))
+            chat.AddText(NameColor, LANG.GetTranslation("killer_you"), White, LANG.GetTranslation("killer_yous"), col, name, White, LANG.GetTranslation("killer_comma"), col, role, LANG.GetTranslation("killer_exclamation"))
         elseif reason == "fell" then
             chat.AddText(NameColor, LANG.GetTranslation("killer_fell"))
         elseif reason == "water" then
--- lua/autorun/server/sv_death_notifier.lua
+++ lua/autorun/server/sv_death_notifier.lua
@@ -1,20 +1,10 @@
-if engine.ActiveGamemode() ~= "terrortown" and gmod.GetGamemode().Name ~= "Trouble in Terrorist Town" then
-    return
-end
-
+if engine.ActiveGamemode() ~= "terrortown" and gmod.GetGamemode().Name ~= "Trouble in Terrorist Town" then return end
 if SERVER then
     util.AddNetworkString("ClientDeathNotify")
-
     local function GetDeathInfo(victim, entity, killer)
         local reason, killerz, role = "nil", "nil", "nil"
-
-        if IsValid(entity) and IsValid(killer) then
-            if entity:GetClass() == "entityflame" and killer:GetClass() == "entityflame" then
-                reason = "burned"
-                entity:Remove()
-            elseif entity:GetClass() == 'prop_physics' or entity:GetClass() == "prop_dynamic" then
-                reason = "prop"
-            elseif killer:IsPlayer() then
+        if IsValid(killer) then
+            if killer:IsPlayer() then
                 if killer == victim then
                     reason = "suicide"
                 else
@@ -23,22 +13,36 @@ if SERVER then
                     role = killer:GetRole()
                 end
             end
-        elseif victim.DiedByWater then
-            reason = "water"
-        elseif entity:GetClass() == "worldspawn" and killer:GetClass() == "worldspawn" then
-            reason = "fell"
         end
 
+        if IsValid(entity) then
+            if entity:GetClass() == "entityflame" then
+                reason = "burned"
+                entity:Remove()
+            elseif entity:GetClass() == 'prop_physics' or entity:GetClass() == "prop_dynamic" then
+                reason = "prop"
+            elseif entity:GetClass() == "worldspawn" then
+                reason = "fell"
+            end
+        end
+
+        if victim.DiedByWater then reason = "water" end
+        if reason == "nil" and victim:IsPlayer() and victim:HasWeapon("weapon_ttt_jihad") then
+            reason = "suicide"
+            killerz = victim:Nick()
+            role = victim:GetRole()
+        end
         return killerz, role, reason
     end
 
+    hook.Add("EntityTakeDamage", "Record_Last_Attacker", function(target, dmginfo) if IsValid(target) and target:IsPlayer() and IsValid(dmginfo:GetAttacker()) then target.LastAttacker = dmginfo:GetAttacker() end end)
     hook.Add("PlayerDeath", "Kill_Reveal_Notify", function(victim, entity, killer)
+        if not IsValid(killer) and IsValid(victim.LastAttacker) then killer = victim.LastAttacker end
         local killerz, role, reason = GetDeathInfo(victim, entity, killer)
-
         net.Start("ClientDeathNotify")
-        net.WriteString(killerz)
-        net.WriteString(role)
-        net.WriteString(reason)
+        net.WriteString(killerz or "Unknown")
+        net.WriteString(tostring(role or "0"))
+        net.WriteString(reason or "unknown")
         net.Send(victim)
     end)
 end
